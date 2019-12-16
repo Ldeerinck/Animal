947266bb4c16f2d084a5ffbaa33eb8ada3a69940
LDeerinck <ldeerinck2@me.com>
2019-12-16 09:54:10 -0800

    Initial Commit

---
+++ Animal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Animal
+//
+//  Created by Luke Deerinck on 12/16/19.
+//  Copyright © 2019 Luke Deerinck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
